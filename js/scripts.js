2c8c38463310407c383292f031f2fb83145df25a
Brooke Fisher <brookenfish1@gmail.com>
2018-11-26 13:45:51 -0500

    Got the OMDB API to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,34 @@
 $(function(){
   console.log('scripts loaded');
-  var myKey = config.MY_KEY;
-  var npsData;
-  var parks = [];
-  var query = '';
-  var msg = '';
+  var myOMBDKey = config.MY_OMDBKEY;
   var html = '';
-
-  // make working search bar
-  $('button').click(function(){
-    //when user clicks submit get the value of the search
-    query = $('#query').val();
-    console.log(query);
-    var url = 'https://api.nps.gov/api/v1/parks?q=' + query + '&api_key=' + myKey;
-    console.log(url);
-
-    //make the AJAX request
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      data: npsData,
-      url: url,
-      success: function(npsData){
-        html = '';
-        parks = npsData.data;
-        console.log(parks);
-        if(parks.length == 0){
-          html += 'No parks matched your query';
-        }
-        parks.forEach(function(park){
-          console.log(park.description);
-          html += '<div><a href="' + park.url + '" target="_blank"><strong>' + park.fullName + '</strong>';
-          html += '<div>' + park.description + '</div><br/></a></div>';
-        });//closing of for each
-        $('#results').html(html);
-      },//end of success
-      error: function(msg){
-        console.log('WTF');
-      }//end of error
-    });//end of AJAX request
-  });//end of button click
-
-  var url2 = 'https://api.fda.gov/drug/event.json?api_key=' + myKey + '&search=oxycontin&limit=100';
-  var fdaData;
-  var drugs = [];
+  var omdbUrl = 'https://www.omdbapi.com/?apikey='+ myOMBDKey + '&s=harry+potter';
+  var movieData = [];
+  var songsData= [];
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: fdaData,
-    url: url2,
-    success: function(fdaData){
-      drugs = fdaData.results;
-      console.log(drugs);
-      drugs.forEach(function(drug){
-        console.log(drug.patient.reaction[0].reactionmeddrapt);
-      });
+    data: movieData,
+    url: omdbUrl,
+    success: function(movieData){
+      console.log(movieData);
     },//end of success
     error: function(msg){
-      console.log('404 error');
+      console.log('You have messed up somewhere');
     }//end of error
   });//end of AJAX request
 
-
+  //second API ajax request
+  // $.ajax({
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   data: songsData,
+  //   url: itunesUrl,
+  //   success: function(songsData){
+  //     console.log(songsData);
+  //   },//end of success
+  //   error: function(msg){
+  //     console.log('Please try again');
+  //   }//end of error
+  // });//end of AJAX request
 });//end of document.ready
