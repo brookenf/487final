ebd3a9ecf60381b92b753699fa7e3d4125428feb
Brooke Fisher <brookenfish1@gmail.com>
2018-11-30 13:52:34 -0500

    Fixed the array so it is arranged by year

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+//YOUTUBE API here
+// Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('movie-trailer', {
+      height: '360',
+      width: '640',
+      videoId: 'nSbzyEJ8X9E'
+    });
+  }
+
+
 $(function(){
   console.log('scripts loaded');
 
@@ -15,13 +34,17 @@ $(function(){
     url: omdbUrl,
     success: function(movieData){
       movies = movieData.Search;
-      console.log(movies);
       moviesHtml += '<h2>A Star is Born</h2>';
+      movies.sort(function(a,b){
+        return a.Year - b.Year
+      });
+      console.log(movies);
       movies.forEach(function(movie){
         if((movie.Year == '1937' ||movie.Year == '1954' || movie.Year == '1976' || movie.Year == '2018') && movie.Poster != 'N/A'){
-          console.log(movie.Poster);
           moviesHtml += '<div class="posters flex">';
-            moviesHtml += '<img class=poster-img alt="A Star is Born poster" src="'+ movie.Poster + '" />';
+            moviesHtml += '<a target="_blank" href="https://www.imdb.com/title/' + movie.imdbID + '/">';
+              moviesHtml += '<img class=poster-img alt="A Star is Born poster" src="'+ movie.Poster + '" />';
+            moviesHtml += '</a>';
           moviesHtml += '</div>';
           $('.movies').html(moviesHtml);
         }
